d0251500acf615f4f38d0d55842577fe5f5a0a59
gaodongyan1 <gaodongyan1@jd.com>
2020-09-13 16:11:17 +0800

    bug:fixed 5.4.x dev tools

--- tasks/build/install_npm_deps.js
+++ tasks/build/install_npm_deps.js
@@ -3,7 +3,8 @@ module.exports = function (grunt) {
   grunt.registerTask('_build:installNpmDeps', function () {
     grunt.file.mkdir('build/kibana/node_modules');
 
-    exec('npm install  --production --no-optional', {
+    // exec('npm install  --production --no-optional', {
+    exec('yarn install --production', {
       cwd: grunt.config.process('<%= root %>/build/kibana')
     }, this.async());
   });
